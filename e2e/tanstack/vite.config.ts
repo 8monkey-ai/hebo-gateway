94c43df49f50fb8ecdd4b6165a4ae642ffcaedd6
Heinrich Wendel <heinrich.wendel@gmail.com>
2026-01-22 08:19:27 +0800

    fix: remove unneeded dep

--- e2e/tanstack/vite.config.ts
+++ e2e/tanstack/vite.config.ts
@@ -1,9 +1,8 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
-  plugins: [nitro(), tanstackStart()],
+  plugins: [tanstackStart()],
 });
 
 export default config;
