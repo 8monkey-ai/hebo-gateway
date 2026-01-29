6e861d58c4845ad756809117e78aa939fe43ce2a
Heinrich Wendel <heinrich.wendel@gmail.com>
2026-01-29 18:51:10 +0800

    fix: correctness

--- src/providers/bedrock/middleware.ts
+++ src/providers/bedrock/middleware.ts
@@ -1,11 +1,7 @@
-import type { CanonicalProviderId } from "../types";
-
 import { createCamelCaseProviderOptionsEmbeddingMiddleware } from "../../middleware/camel-case";
 import { modelMiddlewareMatcher } from "../../middleware/matcher";
 
 // Future: Not for cohere models
 modelMiddlewareMatcher.useForProvider("amazon-bedrock", {
-  embedding: createCamelCaseProviderOptionsEmbeddingMiddleware(
-    "bedrock" satisfies CanonicalProviderId,
-  ),
+  embedding: createCamelCaseProviderOptionsEmbeddingMiddleware("bedrock"),
 });
--- src/providers/cohere/middleware.ts
+++ src/providers/cohere/middleware.ts
@@ -1,10 +1,6 @@
-import type { CanonicalProviderId } from "../types";
-
 import { createCamelCaseProviderOptionsEmbeddingMiddleware } from "../../middleware/camel-case";
 import { modelMiddlewareMatcher } from "../../middleware/matcher";
 
 modelMiddlewareMatcher.useForProvider("cohere.textEmbedding", {
-  embedding: createCamelCaseProviderOptionsEmbeddingMiddleware(
-    "cohere" satisfies CanonicalProviderId,
-  ),
+  embedding: createCamelCaseProviderOptionsEmbeddingMiddleware("cohere"),
 });
