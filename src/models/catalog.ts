ce7afb82cb2622b9f470a85955c869a76c3fabde
Heinrich Wendel <heinrich.wendel@gmail.com>
2026-01-29 08:45:36 +0800

    refactor: more flexible createModelCatalog

--- src/models/catalog.ts
+++ src/models/catalog.ts
@@ -1,5 +1,5 @@
 import type { ModelCatalog } from "./types";
 
-export function createModelCatalog(...entries: ModelCatalog[]): ModelCatalog {
-  return Object.assign({}, ...entries);
+export function createModelCatalog(...entries: Array<ModelCatalog | ModelCatalog[]>): ModelCatalog {
+  return Object.assign({}, ...entries.flat());
 }
